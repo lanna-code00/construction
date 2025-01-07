138c9affd522e94a53d486cca795d88f10b3ab36
= <mezeobidi@trav4college.com>
2025-01-07 15:00:51 +0100

    removed dummy images

--- src/components/About/index.vue
+++ src/components/About/index.vue
@@ -8,13 +8,12 @@
         <div class="sm:flex items-center max-w-screen-xl">
           <div class="sm:w-1/2 p-10">
             <div class="video object-center text-center">
-              <video controls>
-                <source
-                  src="https://res.cloudinary.com/dfl33mmch/video/upload/v1735957357/jmartins/pipe_video_yshq1b.mp4"
-                  type="video/mp4"
-                />
-                Your browser does not support the video tag.
-              </video>
+              <img
+                src="/files/Martins.jpg"
+                class="h-50 w-full"
+                alt=""
+                srcset=""
+              />
             </div>
           </div>
           <div class="sm:w-1/2 p-5">
@@ -53,44 +52,74 @@
           </div>
         </div>
 
-        <div class="lg:mx-10 mx-6 pt-16">
-          <div class="text">
-            <span class="text-black border-b-2 border-black uppercase font-bold"
-              >More about us...</span
-            >
+        <div class="sm:flex items-center max-w-screen-xl">
+          <div class="sm:w-1/2 p-10 lg:hidden flex">
+            <div class="video object-center text-center">
+              <video controls>
+                <source
+                  src="https://res.cloudinary.com/dfl33mmch/video/upload/v1735957357/jmartins/pipe_video_yshq1b.mp4"
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
+            </div>
           </div>
-          <p class="pt-5">
-            For dis our company, we dey focus on one thing wey we sabi
-            well-well: strong rods and metals. Since we start, our mission na to
-            supply builders with materials wey dem fit trust—metals wey go make
-            your project stand strong and last long
-          </p>
 
-          <p class="pt-5">
-            We no dey sell anyhow thing. Na better-quality rods, beams, pipes,
-            and all kinds of metals we dey stock for our warehouse. Whether you
-            dey do small building or big construction project, we get wetin you
-            need to make am solid like rock.
-          </p>
+          <div class="sm:w-1/2 p-10">
+            <div class="text">
+              <span class="text-gray-500 border-b-2 border-black uppercase"
+                >More about us...</span
+              >
+              <!-- <h2 class="my-4 font-bold text-4xl sm:text-4xl">
+                About <span class="text-gray-600">J.Martins</span>
+              </h2> -->
+              <div class="text-gray-700">
+                <p class="pt-5">
+                  For dis our company, we dey focus on one thing wey we sabi
+                  well-well: strong rods and metals. Since we start, our mission
+                  na to supply builders with materials wey dem fit trust—metals
+                  wey go make your project stand strong and last long
+                </p>
 
-          <p class="pt-5">
-            Our customers dey trust us because we no dey disappoint. Materials
-            dey always available, our prices no dey cut throat, and our delivery
-            na sharp-sharp—no delay, no excuse.
-          </p>
+                <p class="pt-5">
+                  We no dey sell anyhow thing. Na better-quality rods, beams,
+                  pipes, and all kinds of metals we dey stock for our warehouse.
+                  Whether you dey do small building or big construction project,
+                  we get wetin you need to make am solid like rock.
+                </p>
 
-          <p class="pt-5">
-            If you get question or you no sure wetin you need, we dey always
-            ready to help. Whether na iron rods for foundation, steel pipes for
-            structure, or beams for support, we go guide you to pick the best
-            materials wey fit your project.
-          </p>
+                <p class="pt-5">
+                  Our customers dey trust us because we no dey disappoint.
+                  Materials dey always available, our prices no dey cut throat,
+                  and our delivery na sharp-sharp—no delay, no excuse.
+                </p>
 
-          <p class="pt-5">
-            We don dey support builders, contractors, and engineers for plenty
-            years now, and we no go stop. Na trust, quality, and customer
-            satisfaction we dey deliver every single time.
-          </p>
+                <p class="pt-5">
+                  If you get question or you no sure wetin you need, we dey
+                  always ready to help. Whether na iron rods for foundation,
+                  steel pipes for structure, or beams for support, we go guide
+                  you to pick the best materials wey fit your project.
+                </p>
+
+                <p class="pt-5">
+                  We don dey support builders, contractors, and engineers for
+                  plenty years now, and we no go stop. Na trust, quality, and
+                  customer satisfaction we dey deliver every single time.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="sm:w-1/2 p-10 hidden lg:flex">
+            <div class="video object-center text-center">
+              <video controls>
+                <source
+                  src="https://res.cloudinary.com/dfl33mmch/video/upload/v1735957357/jmartins/pipe_video_yshq1b.mp4"
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
         </div>
       </div>
     </section>
